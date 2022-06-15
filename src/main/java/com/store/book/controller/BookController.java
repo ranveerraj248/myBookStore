bf8883f57ab6ecff1a3c2efba9565934ef124210
Ranveer Raj <ranveerraj248@gmail.com>
2022-06-16 04:42:48 +0530

    initial commit

---
+++ src/main/java/com/store/book/controller/BookController.java
@@ -0,0 +1,26 @@
+package com.store.book.controller;
+
+import com.store.book.dto.BookDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/books")
+public class BookController {
+
+    @GetMapping
+    public ResponseEntity<List<BookDto>> getBooks(){
+        BookDto book = BookDto.builder()
+                        .title("My First Book Title")
+                        .build();
+        List<BookDto> books = new ArrayList<>();
+        books.add(book);
+
+        return ResponseEntity.ok(books);
+    }
+}
